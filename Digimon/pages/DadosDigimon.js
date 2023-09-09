b92624aab943073f334d324db2c6a8c10a5316e7
pixuleco06 <129403756+pixuleco06@users.noreply.github.com>
2023-09-09 20:03:02 -0300

    Atualizando_projeto

--- Digimon/pages/DadosDigimon.js
+++ Digimon/pages/DadosDigimon.js
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 import DigimonCard from '../Components/DigimonCard';
-import { View, Text, TouchableOpacity, Image, StyleSheet } from 'react-native';
+import { View, Text, TouchableOpacity, Image, StyleSheet, Modal, ScrollView, Dimensions } from 'react-native';
 
 const DadosDigimon = () => {
   const [digimons, setDigimons] = useState([]);
@@ -9,6 +9,7 @@ const DadosDigimon = () => {
   const [currentPage, setCurrentPage] = useState(1);
   const [pageNumber, setPageNumber] = useState(0);
   const [currentDigimons, setCurrentDigimons] = useState([]);
+  const [isModalVisible, setModalVisible] = useState(false);
 
   useEffect(() => {
     async function fetchDigimons() {
@@ -30,6 +31,16 @@ const DadosDigimon = () => {
       setCurrentDigimons(digimons.slice((currentPage - 1) * 8, currentPage * 8));
     }
   }, [currentPage, digimons]);
+
+  const openModal = (digimon) => {
+    setSelectedDigimon(digimon);
+    setModalVisible(true);
+  };
+
+  const closeModal = () => {
+    setSelectedDigimon(null);
+    setModalVisible(false);
+  };
 
   const PagesComponent = () => {
     const [visiblePages, setVisiblePages] = useState(5);
@@ -128,18 +139,30 @@ const DadosDigimon = () => {
             <TouchableOpacity
               style={styles.digimonItem}
               key={digimon.name}
-              onPress={() => setSelectedDigimon(digimon)}
+              onPress={() => openModal(digimon)}
             >
-              {/* Substitua a tag <img> pelo componente <Image> */}
               <Image
-                source={{ uri: digimon.img }} // Certifique-se de que a propriedade 'source' esteja configurada corretamente
-                style={{ width: 100, height: 100 }} // Ajuste o estilo de acordo com suas necessidades
+                source={{ uri: digimon.img }}
+                style={{ width: 100, height: 100 }}
               />
               <Text>{digimon.name}</Text>
             </TouchableOpacity>
           ))}
       </View>
-      {selectedDigimon && <DigimonCard digimon={selectedDigimon} setSelectedDigimon={setSelectedDigimon} />}
+      <Modal animationType="slide" transparent={true} visible={isModalVisible}>
+        <View style={styles.modalContainer}>
+          <View style={styles.modalContent}>
+            <ScrollView style={styles.modalScrollView}>
+              {selectedDigimon && (
+                <DigimonCard digimon={selectedDigimon} setSelectedDigimon={setSelectedDigimon} />
+              )}
+              <TouchableOpacity onPress={closeModal} style={styles.closeButtonContainer}>
+                <Text style={styles.closeButton}>Fechar</Text>
+              </TouchableOpacity>
+            </ScrollView>
+          </View>
+        </View>
+      </Modal>
       <PagesComponent />
     </View>
   );
@@ -150,6 +173,7 @@ const styles = StyleSheet.create({
     fontSize: 24,
     fontWeight: 'bold',
     marginBottom: 20,
+    textAlign: 'center',
   },
   digimonGrid: {
     flexDirection: 'row',
@@ -170,6 +194,33 @@ const styles = StyleSheet.create({
     shadowOpacity: 0.3,
     shadowRadius: 6,
     elevation: 5,
+  },
+  modalContainer: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: 'rgba(0, 0, 0, 0.7)',
+  },
+  modalContent: {
+    backgroundColor: 'white',
+    borderRadius: 10,
+    padding: 20,
+    alignItems: 'center',
+  },
+  closeButtonContainer: {
+    alignItems: 'center',
+  },
+  closeButton: {
+    fontSize: 18,
+    fontWeight: 'bold',
+    marginTop: 10,
+    color: 'white',
+    backgroundColor: 'red',
+    padding: 10,
+    borderRadius: 5,
+  },
+  modalScrollView: {
+    maxHeight: Dimensions.get('window').height - 200,
   },
 });
 
